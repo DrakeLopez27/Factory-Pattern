8ef6a56d854e66a80b82a33c513410f2fe993147
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-02-20 13:34:34 -0600

    ex1

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int NumberOfWheels;
+            bool userInput = false;
+
+            do
+            {
+                Console.WriteLine("Enter the amount of wheels on the vehicle you want created:");
+
+                userInput = int.TryParse(Console.ReadLine(), out NumberOfWheels);
+
+            } while (!userInput);
+
+           var vehicle = VehicleFactory.GetVehicle(NumberOfWheels);
+            vehicle.Drive();
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static  IVehicle GetVehicle(int NumberOfWheels)
+        {
+            if(NumberOfWheels == 4)
+            {
+                return new Car();
+            }
+            else if (NumberOfWheels < 4)
+            {
+                return new Motorcycle();
+            }
+            else
+            {
+                return new Car();
+            }
+        }
+    }
+}
